3680d4fdf5e1f8a88005be750fcd5229fcff62ac
akiogitgit <akimaron2828@icloud.com>
2022-01-06 22:55:43 +0900

    js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// // 美しい
+// const arr = [1, 2, 3, 4, 5];
+// for (i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// // for in 添え字が取得 上と同じ (webpack babel必要？ 昔のブラウザ非対応)
+// for (i in arr) {
+//     console.log(i, arr[i]);
+// }
+
+// // for of 値が取得 上と同じ (webpack babel必要？)
+// for (v of arr) {
+//     console.log(v);
+// }
+
+// let v, i = 0;
+// while (v = arr[i++]) {
+//     console.log(v);
+// }
